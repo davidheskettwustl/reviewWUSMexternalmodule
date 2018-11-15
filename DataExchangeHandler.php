860dbe665390447d45f251564bd77fc957650f54
davidheskettwustl <34380698+davidheskettwustl@users.noreply.github.com>
2018-11-15 15:03:44 -0600

    Pulled out API processing code into its own object

--- DataExchangeHandler.php
+++ DataExchangeHandler.php
@@ -14,8 +14,6 @@
 
 namespace WashingtonUniversity\ProjectToProjectFieldReplicationExternalModule;
 
-include_once 'CurlCommunications.php';
-
 use REDCap;
 
 /** 
@@ -27,54 +25,22 @@ class DataExchangeHandler
 	public $flagError;
 	public $flagErrorType;
 	
-	private $commLink;
+	protected $errMsgs;
+	protected $flagOverWriteBlanks;
 
-	private $destToken;
-	private $sorcToken;
-	
-	private $flagOverWriteBlanks;
-
-	CONST ERR_NO_COMM      = 1;
-	CONST ERR_NO_SRC_TOKEN = 2;
-	CONST ERR_NO_DST_TOKEN = 3;
-	
 	/**
 	 * - set up our defaults.
 	 */
 	function __construct()
 	{
-		$this->commLink = null;
 		$this->flagError = null;
 		$this->flagErrorType = 0;
 		$this->flagOverWriteBlanks = false;
-		$this->destToken = null;
-		$this->sorcToken = null;
 		$this->message = '';
 
 		$this->errMsgs[] = 'Clear';
-		$this->errMsgs[] = 'No Curl Comms';
-		$this->errMsgs[] = 'No Source Token';
-		$this->errMsgs[] = 'No Destination Token';
 	}
 		
-	/**
-	 * setComm - set the curl comm.
-	 */
-	public function setComm($commObj)
-	{
-		$this->commLink = $commObj;
-	}
-
-	/**
-	 * setComm - set the curl comm.
-	 */
-	public function setCommApiUrl($apiUrl)
-	{
-		if ($this->commLink) {
-			$this->commLink->setApiUrl($apiUrl);
-		}
-	}
-
 	/**
 	 * getErrorMsg - get the error message.
 	 */
@@ -114,22 +80,6 @@ class DataExchangeHandler
 	{
 		return $this->message;
 	}
-	
-	/**
-	 * setComm - set the curl comm.
-	 */
-	public function setTokenSorc($token)
-	{
-		$this->sorcToken = $token;
-	}
-
-	/**
-	 * setComm - set the curl comm.
-	 */
-	public function setTokenDest($token)
-	{
-		$this->destToken = $token;
-	}
 
 	/**
 	 * setOverWriteBlanks - set the flag as per REDCap::saveData, false = 'normal', true = 'overwrite'. All blank values will be ignored and will not be saved (existing saved values will be kept), but with 'overwrite', any blank values will overwrite any existing saved data. By default, 'normal' is used..
@@ -139,105 +89,6 @@ class DataExchangeHandler
 		$this->flagOverWriteBlanks = $flag;
 	}
 	
-	/**
-	 * getSourceDataApi - get source data.
-	 */
-	public function getSourceDataApi($recordsArray, $formsArray, $eventsArray)  // read source
-	{
-		if (!$this->commLink) {
-			$this->flagError = true;
-			$this->flagErrorType = self::ERR_NO_COMM;
-			$this->addErrorMsg();
-		}
-
-		if (!$this->sorcToken) {
-			$this->flagError = true;
-			$this->flagErrorType = self::ERR_NO_SRC_TOKEN;
-			$this->addErrorMsg();
-		}
-		
-		if ($this->flagError) {
-			$this->addErrorMsg('getSourceDataApi');
-			return null;
-		}
-
-		$apiQueryData = array(
-		    'token'                  => $this->sorcToken,
-		    'content'                => 'record',
-		    'format'                 => 'json',
-		    'type'                   => 'eav',
-		    'records'                => $recordsArray,
-		    'forms'                  => $formsArray,
-		    'events'                 => $eventsArray,
-		    'rawOrLabel'             => 'raw',
-		    'rawOrLabelHeaders'      => 'raw',
-		    'exportCheckboxLabel'    => 'false',
-		    'exportSurveyFields'     => 'false',
-		    'exportDataAccessGroups' => 'false',
-		    'returnFormat'           => 'json'
-		);
-		
-		$output = $this->commLink->communicateCurl($apiQueryData);
-
-		if ($output == null) {
-			$this->flagError = true;
-			$this->addErrorMsg($this->commLink->getMessage());
-			return null;
-		}
-				
-		return $output;
-	}
-
-	/**
-	 * putDestinationApi - put destination data.
-	 */
-	public function putDestinationApi($data)  // write destination
-	{
-		if (!$this->commLink) {
-			$this->flagError = true;
-			$this->flagErrorType = self::ERR_NO_COMM;
-			$this->addErrorMsg();
-		}
-
-		if (!$this->destToken) {
-			$this->flagError = true;
-			$this->flagErrorType = self::ERR_NO_DST_TOKEN;
-			$this->addErrorMsg();
-		}
-
-		if ($this->flagError) {
-			$this->addErrorMsg('putDestinationApi');
-			return null;
-		}
-
-		$overwriteState = 'normal';
-		if ($this->flagOverWriteBlanks) {
-			$overwriteState = 'overwrite';
-		}
-		
-		$apiQueryData = array(
-		    'token'             => $this->destToken,
-		    'content'           => 'record',
-		    'format'            => 'json',
-		    'type'              => 'eav',
-		    'overwriteBehavior' => $overwriteState,
-		    'forceAutoNumber'   => 'false',
-		    'data'              => $data,
-		    'returnContent'     => 'count',
-		    'returnFormat'      => 'json'
-		);
-
-		$output = $this->commLink->communicateCurl($apiQueryData);
-		
-		if ($output == null) {
-			$this->flagError = true;
-			$this->addErrorMsg($this->commLink->getMessage());
-			return null;
-		}
-		
-		return $output;
-	}
-
 	/**
 	 * getSourceRedcapData - get source data, using REDCap::getData method.
 	 */
